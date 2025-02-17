462915b05c8534d2663d6597054b999fa24f4c3b
Nathan James <nathanjms@outlook.com>
2025-02-17 17:12:47 +0000

    cors via env

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"strconv"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/nathanjms/go-google-sheets/internal/application"
+	"github.com/nathanjms/go-google-sheets/internal/env"
 )
 
 func serveHttp(app *application.Application) error {
 	e := echo.New()
+
+	originsFromEnv := env.GetString("ALLOWED_ORIGINS_BY_COMMA", "http://localhost:3000")
+	allowedOrigins := []string{}
+
+	for _, origin := range strings.Split(originsFromEnv, ",") {
+		allowedOrigins = append(allowedOrigins, strings.Trim(origin, " "))
+	}
+
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{"http://localhost:3000", "https://*.nathanjms.co.uk"},
+		AllowOrigins:     allowedOrigins,
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderContentEncoding},
 		AllowCredentials: true,
 	}))
